ab4cd50f3b0af7f7de9a0489a80016b5e04d1fa8
Nhan0504 <ttn0504ltt@gmail.com>
2022-08-16 22:43:12 +0700

    Add the controller to get the list of customers from the database

---
+++ Back_end/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Back_end.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_end.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            using (var context = new CRMContext())
+            {
+                return context.Customers.ToList();
+            }
+        }
+    }
+}
