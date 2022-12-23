75ed23d0ddf5c622b6c65bfe6ac82bb981dc83c7
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-23 16:01:13 -0500

    Fix the base url

---
+++ Front_end/ClientApp/src/app/shared/baseurl.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.baseURL = void 0;
+exports.baseURL = "http://localhost:5000/api";
+//# sourceMappingURL=baseurl.js.map
