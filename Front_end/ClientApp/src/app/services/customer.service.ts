f3cd695ab749ef18d03da419f8a7fc87d78fe875
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-23 16:02:56 -0500

    Add the service to retrieve customers data from the server

---
+++ Front_end/ClientApp/src/app/services/customer.service.ts
@@ -0,0 +1,54 @@
+﻿import { Injectable } from '@angular/core';
+import { Customer } from '../shared/customer';
+import { baseURL } from '../shared/baseurl';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CustomerService {
+
+    constructor(private http: HttpClient) { }
+
+    //Get all customers
+    getCustomers(): Observable<Customer[]> {
+        return this.http.get<Customer[]>(baseURL + 'Customers');
+    }
+
+    //Get a customer by ID
+    getCustomer(id: string): Observable<Customer> {
+        return this.http.get<Customer>(baseURL + 'Customers/' + id);
+    }
+
+    //Get a customer's total number of transactions
+    getCustomerTotaltrans(id: string): Observable<Customer> {
+        return this.http.get<Customer>(baseURL + 'Customers/' + id + '/totaltrans');
+    }
+
+    //Get a customer's all transactions details
+    getCustomerTransaction(id: string): Observable<Customer> {
+        return this.http.get<Customer>(baseURL + 'Customers/' + id + '/transaction');
+    }
+
+    //getFeaturedDish(): Observable<Customer> {
+    //    return this.http.get<Customer[]>(baseURL + 'customers?featured=true')
+    //        .pipe(map(customers => customers[0]));
+    //}
+
+    //getDishIds(): Observable<string[] | any> {
+    //    return this.getCustomers().pipe(map(dishes => dishes.map(customer => customer.ID)))
+    //        .pipe(catchError(error => error));
+    //}
+
+    //putCustomer(customer: Customer): Observable<Customer> {
+    //    const httpOptions = {
+    //        headers: new HttpHeaders({
+    //            'content-Type': 'application/json'
+    //        })
+    //    }
+    //    return this.http.put<Customer>(baseURL + 'customers/' + customer.ID, customer, httpOptions);
+    //}
+
+}
