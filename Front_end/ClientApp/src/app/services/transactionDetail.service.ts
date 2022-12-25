4a64e2ef59f72c587c671dbf7ac4beaf887d7e56
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-25 09:19:45 -0500

    Add the service to fetch transaction detail data

---
+++ Front_end/ClientApp/src/app/services/transactionDetail.service.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable } from '@angular/core';
+import { TransactionDetail } from '../shared/transactionDetail';
+import { baseURL } from '../shared/baseurl';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TransactionDetailService {
+
+    constructor(private http: HttpClient) { }
+
+    //Get all transactions detail
+    getTransactionsDetail(): Observable<TransactionDetail[]> {
+        return this.http.get<TransactionDetail[]>(baseURL + 'transactionsdetails');
+    }
+
+    //Add transaction detail
+    postTransactionDetail(transactionDetail: TransactionDetail): Observable<TransactionDetail> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'content-Type': 'application/json'
+            })
+        }
+        return this.http.post<TransactionDetail>(baseURL + 'transactiondetails/', transactionDetail, httpOptions);
+    }
+}
