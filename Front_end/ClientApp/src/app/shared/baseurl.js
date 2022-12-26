2630b587d3c8b8a0d9230b626f5825ecf9b0011c
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-26 09:33:27 -0500

    Remove build file

--- Front_end/ClientApp/src/app/shared/baseurl.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseURL = void 0;
-exports.baseURL = "http://localhost:5000/api";
-//# sourceMappingURL=baseurl.js.map
