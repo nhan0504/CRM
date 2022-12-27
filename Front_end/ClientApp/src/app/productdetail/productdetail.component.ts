694a18a520193001ef4fa900413c21b0fb4d8c64
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-27 08:51:06 -0500

    Create product detail component to show detail of a product

---
+++ Front_end/ClientApp/src/app/productdetail/productdetail.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit, Inject } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Product } from '../shared/product';
+import { ProductService } from '../services/product.service';
+
+@Component({
+    selector: 'app-productdetail-component',
+    templateUrl: './productdetail.component.html'
+})
+export class ProductdetailComponent implements OnInit {
+
+    id: string;
+    product: Product;
+
+    constructor(private route: ActivatedRoute,
+        private productService: ProductService,
+        @Inject('baseURL') public baseURL: string) { }
+
+    ngOnInit(): void {
+        this.route.params.subscribe(params => this.id = params.id);
+        this.productService.getProduct(this.id)
+            .subscribe(product => this.product = product);
+    }
+
+}
