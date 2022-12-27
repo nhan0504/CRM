e12ac6326a4d176e565eec110357a2f615e05716
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-26 19:09:40 -0500

    Get the static id param in the url

--- Front_end/ClientApp/src/app/customerdetail/customerdetail.component.ts
+++ Front_end/ClientApp/src/app/customerdetail/customerdetail.component.ts
@@ -1,13 +1,28 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Customer } from '../shared/customer';
+import { CustomerService } from '../services/customer.service';
 
 @Component({
     selector: 'app-customerdetail-component',
     templateUrl: './customerdetail.component.html'
 })
-export class CustomerdetailComponent {
-    public currentCount = 0;
+export class CustomerdetailComponent implements OnInit {
+    //customer!: Customer | null;
+    //customerIds!: number[];
+    id: String;
 
-    public incrementCounter() {
-        this.currentCount++;
+    //constructor(private customerService: CustomerService,
+    //    @Inject('baseURL') public baseURL: string) {
+    //}
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+        this.route.params.subscribe(params => {
+            console.log('The id of this route is: ', params.id);
+        });
+        //this.customerService.getCustomerIds()
+        //    .subscribe(customerIds => this.customerIds = customerIds);
     }
+
 }
