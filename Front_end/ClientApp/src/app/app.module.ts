c925ca3caa62b3072d960213a404a9310313f15f
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-27 08:51:57 -0500

    Add the route for product detail component

--- Front_end/ClientApp/src/app/app.module.ts
+++ Front_end/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { CustomerComponent } from './Customer/customer.component';
 import { CustomerdetailComponent } from './customerdetail/customerdetail.component';
+import { ProductdetailComponent } from './productdetail/productdetail.component';
 
 import { CustomerService } from './services/customer.service';
 import { baseURL } from './shared/baseurl';
@@ -23,7 +24,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
     HomeComponent,
     ProductComponent,
     CustomerComponent,
-    CustomerdetailComponent
+    CustomerdetailComponent,
+    ProductdetailComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -32,6 +34,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'product', component: ProductComponent },
+      { path: 'productdetail/:id', component: ProductdetailComponent },
       { path: 'customer', component: CustomerComponent },
       { path: 'customerdetail/:id', component: CustomerdetailComponent },
     ]),
