db9b5c80b18458a26fdaf37734ec413b54b1ffab
Nhan0504 <ttn0504ltt@gmail.com>
2022-12-26 21:02:55 -0500

    Get the customer by ID

--- Front_end/ClientApp/src/app/customerdetail/customerdetail.component.ts
+++ Front_end/ClientApp/src/app/customerdetail/customerdetail.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, Inject } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Customer } from '../shared/customer';
 import { CustomerService } from '../services/customer.service';
@@ -10,19 +10,18 @@ import { CustomerService } from '../services/customer.service';
 export class CustomerdetailComponent implements OnInit {
     //customer!: Customer | null;
     //customerIds!: number[];
-    id: String;
 
-    //constructor(private customerService: CustomerService,
-    //    @Inject('baseURL') public baseURL: string) {
-    //}
-    constructor(private route: ActivatedRoute) { }
+    id: string;
+    customer: Customer;
+
+    constructor(private route: ActivatedRoute,
+        private customerService: CustomerService,
+        @Inject('baseURL') public baseURL: string) { }
 
     ngOnInit(): void {
-        this.route.params.subscribe(params => {
-            console.log('The id of this route is: ', params.id);
-        });
-        //this.customerService.getCustomerIds()
-        //    .subscribe(customerIds => this.customerIds = customerIds);
+        this.route.params.subscribe(params =>  this.id = params.id);
+        this.customerService.getCustomer(this.id)
+            .subscribe(customer => this.customer = customer);
     }
 
 }
